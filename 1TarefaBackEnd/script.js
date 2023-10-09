1e670a042e9e417c50cfa821abed5b4cbe46b3ee
André Luiz <dantasluizandre@gmail.com>
2023-10-08 22:39:35 -0300

    fazendo

---
+++ 1TarefaBackEnd/script.js
@@ -0,0 +1,17 @@
+//1° //
+const express = require('express');
+//2°//
+const app = express()
+//3°//
+app.use(express.json());
+//4°//
+app.listen(3000 , () => {
+    console.log("servidor iniciado")
+})
+//==============CRIAR APIs===============================
+// GET / GET BY ID // 
+
+app.get('/usuarios' (req,res) => {
+    console.log("APi GET todos")
+    res.status(200).send("todos os usuarios")
+})
